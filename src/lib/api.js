7db3555c04670f02b197c7a5fccb60448640a8b2
360-ahemenjeho <ahemenjeho2312@outlook.com>
2026-03-10 16:46:22 +0100

    Change api base url

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { notification } from "./notification";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "http://localhost:4000/V1/api",
   withCredentials: true,
 });
 
